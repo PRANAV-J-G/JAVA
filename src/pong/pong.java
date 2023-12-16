7622dc5e8e69098c1389453db16768927443693f
PRANAV-J-G <pranvjg03@gmail.com>
2023-12-16 08:18:55 +0530

    first commit

---
+++ src/pong/pong.java
@@ -0,0 +1,16 @@
+package pong;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class pong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+GameFrame frame = new GameFrame();
+
+	}
+
+}
